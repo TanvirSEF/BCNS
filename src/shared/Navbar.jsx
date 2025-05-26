e60d00cfef6892298e2a8bbd84f2829f9a283f01
jihad-islam <jihadislam.diu@gmail.com>
2025-05-26 12:07:11 +0600

    added in navbar

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <header className="flex flex-col items-center justify-center gap-4 p-4 bg-[#F0F0F0]">
+        <div>
+          <img src="/logo.png" alt="Logo" />
+        </div>
 
-export default Navbar
+        <div>
+          <h1 className="text-[#372250] font-bold">
+            বাংলাদেশ চাইল্ড নিউরোলজি সোসাইটি (বিসিএনস)
+          </h1>
+          <h1 className="text-[#4B9FBE] font-bold">
+            Bangladesh Child Neurology Society (BCNS)
+          </h1>
+          <p>
+            যোগাযোগ: রুম নং-৭০৩, ব্লক-এফ, ৭ম তলা, ইপনা, বিএমইউ, শাহবাগ, ঢাকা
+          </p>
+        </div>
+
+        <div>
+          <p>Website: www.bcns.org.bd</p>
+          <p>E-mail: office@bcns.org.bd</p>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
