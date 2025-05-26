2215ce24b4b2159de46375c480fe824a5241a32b
jihad-islam <jihadislam.diu@gmail.com>
2025-05-26 12:01:33 +0600

    added colors on some texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,12 @@ const Home = () => {
         </div>
 
         <div>
-          <h1>বাংলাদেশ চাইল্ড নিউরোলজি সোসাইটি (বিসিএনস)</h1>
-          <h1>Bangladesh Child Neurology Society (BCNS)</h1>
+          <h1 className="text-[#372250] font-bold">
+            বাংলাদেশ চাইল্ড নিউরোলজি সোসাইটি (বিসিএনস)
+          </h1>
+          <h1 className="text-[#4B9FBE] font-bold">
+            Bangladesh Child Neurology Society (BCNS)
+          </h1>
           <p>
             যোগাযোগ: রুম নং-৭০৩, ব্লক-এফ, ৭ম তলা, ইপনা, বিএমইউ, শাহবাগ, ঢাকা
           </p>
